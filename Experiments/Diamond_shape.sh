be0d4f2fd439f81dde069e52d1a9cb19e2c79d82
Shaurya Upadhyay <shaurya01836@gmail.com>
2025-04-02 22:51:21 +0530

    Update Diamond_shape.sh

--- Experiments/Diamond_shape.sh
+++ Experiments/Diamond_shape.sh
@@ -1,35 +1,34 @@
 row=5
-n=1
-#upper part
-for ((i=1 ; i<=row; i=`expr $i + 2`))
+n=`expr $row / 2`
+
+# Upper part
+for ((i=1; i<=row; i=`expr $i + 2`))
 do 	
-	for (( k=n ; k<=2; k++ ))
+	for (( k=1; k<=n; k++ ))
 	do
-		echo -n " "		
+		echo -n " "
 	done
-	for (( j=1 ; j<=i; j++ ))
+	for (( j=1; j<=i; j++ ))
 	do	
 		echo -n "*"
 	done
-	echo " "
-	n=`expr $n + 1`
-done
-
-
-#lower part
-n=2
-row=`expr $row - 2`
-for ((i=row ; i>=1; i=`expr $i - 2`))
-do 
-	for (( k=n ; k<=2; k++ ))
-	do
-		echo -n " "		
-	done
-	for (( j=1 ; j<=i; j++ ))
-	do
-		echo -n "*"
-	done
-	echo " "
+	echo ""
 	n=`expr $n - 1`
 done
 
+# Lower part
+n=1
+row=`expr $row - 2`
+for ((i=row; i>=1; i=`expr $i - 2`))
+do 
+	for (( k=1; k<=n; k++ ))
+	do
+		echo -n " "
+	done
+	for (( j=1; j<=i; j++ ))
+	do
+		echo -n "*"
+	done
+	echo ""
+	n=`expr $n + 1`
+done
